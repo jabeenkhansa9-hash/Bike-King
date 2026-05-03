35405afd2619d19bffcab9afd33a77889dfea7fe
jabeenkhansa9-hash <jabeenkhansa9@gmail.com>
2026-05-03 18:43:01 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'bike-king-v1';
+const urlsToCache = ['/Bike-King/', '/Bike-King/index.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
